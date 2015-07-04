1d7b23b9f1531fe2fbd69e439726228171adc66e
Nikola Plavsic <nikolaplavisc@gmail.com>
2015-07-04 21:58:59 +0200

    Mapper trait

--- Events/EventDispatcher.php
+++ Events/EventDispatcher.php
@@ -47,8 +47,13 @@ class EventDispatcher extends Dispatcher
         // When the given "event" is actually an object we will assume it is an event
         // object and use the class as the event name and this event itself as the
         // payload to the handler, which makes object based events quite simple.
-        if (is_object($event)) {
-            list($payload, $event) = [[$event], get_class($event)];
+        if ( is_object($event) )
+        {
+            list($payload, $event, $eventName) = [ [$event], $event, get_class($event) ];
+        }
+        else
+        {
+            $eventName = $event;
         }
 
         $responses = [];
@@ -56,17 +61,20 @@ class EventDispatcher extends Dispatcher
         // If an array is not given to us as the payload, we will turn it into one so
         // we can easily use call_user_func_array on the listeners, passing in the
         // payload to each of them so that they receive each of these arguments.
-        if (!is_array($payload)) {
+        if ( ! is_array($payload) )
+        {
             $payload = [$payload];
         }
 
-        $this->firing[] = $event;
+        $this->firing[] = $eventName;
 
-        if (isset($payload[0]) && $payload[0] instanceof ShouldBroadcast) {
+        if ( isset($payload[0]) && $payload[0] instanceof ShouldBroadcast )
+        {
             $this->broadcastEvent($payload[0]);
         }
 
-        foreach ($this->getListeners($event) as $listener) {
+        foreach ($this->getListeners($event) as $listener)
+        {
         	
         	try
         	{
@@ -81,7 +89,8 @@ class EventDispatcher extends Dispatcher
             // If a response is returned from the listener and event halting is enabled
             // we will just return this response, and not call the rest of the event
             // listeners. Otherwise we will add the response on the response list.
-            if (!is_null($response) && $halt) {
+            if (!is_null($response) && $halt)
+            {
                 array_pop($this->firing);
 
                 return $response;
@@ -90,7 +99,8 @@ class EventDispatcher extends Dispatcher
             // If a boolean false is returned from a listener, we will stop propagating
             // the event to any further listeners down in the chain, else we keep on
             // looping through the listeners and firing every one in our sequence.
-            if ($response === false) {
+            if ($response === false)
+            {
                 break;
             }
 
@@ -106,5 +116,57 @@ class EventDispatcher extends Dispatcher
         }
 
         return $halt ? null : $responses;
+    }
+
+
+
+    /**
+     * Get all of the listeners for a given event.
+     *
+     * @param  mixed  $event
+     * @return array
+     */
+    public function getListeners($event)
+    {
+        $parentListeners = [];
+        $wildcards = [];
+
+        if( is_object($event) )
+        {
+            $eventName = get_class($event);
+            $parentListeners = $this->getParentListeners($event);
+        }
+        else
+        {
+            $eventName = $event;
+            $wildcards = $this->getWildcardListeners($event);
+        }
+
+        if (!isset($this->sorted[$eventName])) {
+            $this->sortListeners($eventName);
+        }
+
+        return array_merge($this->sorted[$eventName], $wildcards, $parentListeners);
+    }
+
+    /**
+     * Get the parent class listeners for the event.
+     *
+     * @param  object  $event
+     * @return array
+     */
+    protected function getParentListeners($event)
+    {
+        $parentListeners = [];
+
+        foreach ($this->listeners as $key => $listeners)
+        {
+            if ($event instanceof $key)
+            {
+                $parentListeners = array_merge($parentListeners, $listeners);
+            }
+        }
+
+        return $parentListeners;
     }
 }
---
+++ Traits/MapperTrait.php
@@ -0,0 +1,104 @@
+<?php 
+/*
+ * This file is part of the cookbook/core package.
+ *
+ * (c) Nikola Plavšić <nikolaplavsic@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cookbook\Core\Traits;
+
+/**
+ * MapperTrait for mapping events/handlers/commands
+ * 
+ * Gives class ability to map events/handlers/commands
+ * 
+ * @author  	Nikola Plavšić <nikolaplavsic@gmail.com>
+ * @copyright  	Nikola Plavšić <nikolaplavsic@gmail.com>
+ * @package 	cookbook/core
+ * @since 		0.1.0-alpha
+ * @version  	0.1.0-alpha
+ */
+trait MapperTrait
+{
+
+	/**
+	 * All of the mappings.
+	 *
+	 * @var array
+	 */
+	protected static $mappings = [];
+
+	/**
+	 * The fallback mapping Closures.
+	 *
+	 * @var array
+	 */
+	protected static $mappers = [];
+
+	/**
+	 * Register mappings.
+	 *
+	 * @param  array  $mappings
+	 * @return void
+	 */
+	public static function map(array $mappings, $key = 'default')
+	{
+		self::$mappings = array_merge_recursive(self::$mappings, [ $key => $mappings ]);
+	}
+
+	/**
+	 * Register a fallback mapper callback.
+	 *
+	 * @param  \Closure  $mapper
+	 * @return void
+	 */
+	public static function mapper(Closure $mapper, $key = 'default')
+	{
+		self::$mappers[$key] = $mapper;
+	}
+
+	/**
+	 * Get mappings for the resource.
+	 *
+	 * @param  mixed  $command
+	 * @return string
+	 */
+	public static function getMappings($resource, $key = 'default')
+	{
+		$resourceName = $resource;
+
+		if( is_object($resource) )
+		{
+			$resourceName = get_class($resource);
+		}
+
+		$mappings = [];
+		if( isset(self::$mappers[$key]) )
+		{
+			$mappings = array_merge_recursive( $mappings, call_user_func(self::$mappers[$key], $resourceName) );
+		}
+
+		if( isset(self::$mappings[$key]) && isset(self::$mappings[$key][$resourceName]) )
+		{
+			$mappings = array_merge_recursive( $mappings, self::$mappings[$key][$resourceName] );
+		}
+
+		return $mappings;
+	}
+
+	/**
+	 * Get mapping segment.
+	 *
+	 * @param  string  	$mapping
+	 * @param  int  	$segment
+	 * @return string
+	 */
+	protected static function getSegment($mapping, $segment)
+	{
+		return explode('@', $mapping)[$segment];
+	}
+
+}
