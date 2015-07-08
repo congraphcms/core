7d35c2490dc276b30971d263c71369b141a7c795
Nikola Plavsic <nikolaplavisc@gmail.com>
2015-07-08 15:29:54 +0200

    small fix

--- helpers.php
+++ helpers.php
@@ -20,3 +20,63 @@ if (!function_exists('is_assoc')) {
 		return array_keys($array) !== range(0, count($array) - 1);
 	}
 }
+
+if (!function_exists('cbMergeCollections')) {
+	/**
+	 * Merge collections of cookbook objects
+	 *
+	 * @param  array  $array
+	 * @return boolean
+	 */
+	function cbMergeCollections(array $col_1, array $col_2)
+	{
+		$args = func_get_args();
+
+		$merged = [];
+		foreach ($args as $col) {
+			if( ! is_array($col) )
+			{
+				throw new InvalidArgumentsException('cbMergeCollections expects all parameters to be arrays.');
+			}
+
+			foreach ($col as $obj) {
+				if( ! cbInCollection($merged, $obj->id, $objt->type) )
+				{
+					$merged[] = $obj;
+				}
+			}
+		}
+
+		return $merged;
+	}
+}
+
+if (!function_exists('cbInCollection')) {
+	/**
+	 * Check if object is in collection
+	 *
+	 * @param  array  	$array
+	 * @param  int  	$id
+	 * @param  string  	$type
+	 * @return boolean
+	 */
+	function cbInCollection(array $col, $id, $type = null)
+	{
+		foreach ($col as $colObject) {
+			if($colObject->id == $id)
+			{
+				if($type !== null)
+				{
+					if($colObject->type == $type)
+					{
+						return true;
+					}
+					continue;
+				}
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
