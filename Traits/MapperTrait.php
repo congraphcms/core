1c7761e296fa769e3f9b2d3ea3a9e3ecdb4b8a0f
Nikola Plavsic <nikolaplavisc@gmail.com>
2015-07-16 20:20:09 +0200

    small fix

--- Traits/MapperTrait.php
+++ Traits/MapperTrait.php
@@ -83,6 +83,7 @@ trait MapperTrait
 
 		if( isset(self::$mappings[$key]) && isset(self::$mappings[$key][$resourceName]) )
 		{
+			dd(self::$mappings[$key]);
 			$mappings = array_merge_recursive( $mappings, self::$mappings[$key][$resourceName] );
 		}
 
