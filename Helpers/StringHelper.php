f95986f2dc8db225b006216dffc4058c9451648f
Nikola Plavsic <nikolaplavisc@gmail.com>
2018-05-02 17:29:35 +0200

    StringHelper seems_utf8 issue on new php fixed

--- Helpers/StringHelper.php
+++ Helpers/StringHelper.php
@@ -70,7 +70,7 @@ class StringHelper implements StringHelperContract
 	public static function remove_accents( $string ) {
 		if ( !preg_match('/[\x80-\xff]/', $string) )
 			return $string;
-		if (seems_utf8($string)) {
+		if (self::seems_utf8($string)) {
 			$chars = array(
 			// Decompositions for Latin-1 Supplement
 			chr(194).chr(170) => 'a', chr(194).chr(186) => 'o',
@@ -270,4 +270,39 @@ class StringHelper implements StringHelperContract
 		}
 		return $string;
 	}
+
+	/**
+	 * Checks to see if a string is utf8 encoded.
+	 *
+	 * NOTE: This function checks for 5-Byte sequences, UTF8
+	 *       has Bytes Sequences with a maximum length of 4.
+	 *
+	 * @author bmorel at ssi dot fr (modified)
+	 * @since 1.2.1
+	 *
+	 * @param string $str The string to be checked
+	 * @return bool True if $str fits a UTF-8 model, false otherwise.
+	 */
+	protected static function seems_utf8($str)
+	{
+		mbstring_binary_safe_encoding();
+		$length = strlen($str);
+		reset_mbstring_encoding();
+		for ($i = 0; $i < $length; $i++) {
+			$c = ord($str[$i]);
+			if ($c < 0x80) $n = 0; // 0bbbbbbb
+			elseif (($c & 0xE0) == 0xC0) $n = 1; // 110bbbbb
+			elseif (($c & 0xF0) == 0xE0) $n = 2; // 1110bbbb
+			elseif (($c & 0xF8) == 0xF0) $n = 3; // 11110bbb
+			elseif (($c & 0xFC) == 0xF8) $n = 4; // 111110bb
+			elseif (($c & 0xFE) == 0xFC) $n = 5; // 1111110b
+			else return false; // Does not match any model
+			for ($j = 0; $j < $n; $j++) { // n bytes matching 10bbbbbb follow ?
+				if ((++$i == $length) || ((ord($str[$i]) & 0xC0) != 0x80))
+					return false;
+			}
+		}
+		return true;
+	}
+
 }
