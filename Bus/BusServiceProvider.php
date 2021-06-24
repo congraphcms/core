097a3ba7f22f7d0377aa5c51ba310b5a88d431ad
Nikola Plavsic <nikola.plavsic@sbgenomics.com>
2021-06-24 12:43:55 +0200

    laravel 5.3

--- Bus/BusServiceProvider.php
+++ Bus/BusServiceProvider.php
@@ -6,6 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class BusServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
 
     /**
@@ -15,18 +21,43 @@ class BusServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        var_dump("REGISTER");
         $this->app->singleton('Congraph\Core\Bus\CommandDispatcher', function ($app) {
             return new CommandDispatcher($app, function () use ($app) {
                 return $app['Illuminate\Contracts\Queue\Queue'];
             });
         });
 
-        $this->app->alias(
-            'Congraph\Core\Bus\CommandDispatcher', 'Illuminate\Contracts\Bus\Dispatcher'
+        $this->app->bind(
+            'Illuminate\Contracts\Bus\Dispatcher',
+            'Congraph\Core\Bus\CommandDispatcher'
         );
 
-        $this->app->alias(
-            'Congraph\Core\Bus\CommandDispatcher', 'Illuminate\Contracts\Bus\QueueingDispatcher'
+        $this->app->bind(
+            'Illuminate\Contracts\Bus\QueueingDispatcher',
+            'Congraph\Core\Bus\CommandDispatcher'
         );
+
+        // $this->app->alias(
+        //     'Congraph\Core\Bus\CommandDispatcher', 'Illuminate\Contracts\Bus\Dispatcher'
+        // );
+
+        // $this->app->alias(
+        //     'Congraph\Core\Bus\CommandDispatcher', 'Illuminate\Contracts\Bus\QueueingDispatcher'
+        // );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'Congraph\Core\Bus\CommandDispatcher',
+            'Illuminate\Contracts\Bus\Dispatcher',
+            'Illuminate\Contracts\Bus\QueueingDispatcher',
+        ];
     }
 }
---
+++ tests/Bus/DispatcherTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Debug\Dumper;
+
+class DispatcherTest extends Orchestra\Testbench\TestCase
+{
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		// code here
+		$this->d = new Dumper();
+	}
+
+	public function tearDown()
+	{
+		// code here
+
+		parent::tearDown();
+	}
+
+		/**
+	 * Define environment setup.
+	 *
+	 * @param  \Illuminate\Foundation\Application  $app
+	 *
+	 * @return void
+	 */
+	protected function getEnvironmentSetUp($app)
+	{
+		$app['config']->set('database.default', 'testbench');
+		$app['config']->set('database.connections.testbench', [
+			'driver'   	=> 'mysql',
+			'host'      => '127.0.0.1',
+			'port'		=> '3306',
+			'database'	=> 'congraph_testbench',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		]);
+
+	}
+
+	protected function getPackageProviders($app)
+	{
+		return ['Congraph\Core\CoreServiceProvider'];
+	}
+
+	public function testTheTest()
+	{
+		fwrite(STDOUT, __METHOD__ . "\n");
+	}
+
+	public function testLoadingOfDispatcher()
+	{
+		fwrite(STDOUT, __METHOD__ . "\n");
+
+		$bus = $this->app->make('Congraph\Core\Bus\CommandDispatcher');
+		$this->d->dump(get_class($bus));
+		$this->assertEquals('Congraph\Core\Bus\CommandDispatcher', get_class($bus));
+	}
+
+}
